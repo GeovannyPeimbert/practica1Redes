2720c0ad1f98122ee404e9eedfa6a81ff4db2881
GeovannyPeimbert <giovocatrece@gmail.com>
2021-06-07 09:17:17 -0500

    cambio texto

--- HttpTriggerPractica1/index.js
+++ HttpTriggerPractica1/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
     context.log('Generating Nonsense...');
     context.res = {
-        body: "Bibble wibble wobble"
+        body: "Hola, soy Gio, practica 1 de redes"
     };
 };
